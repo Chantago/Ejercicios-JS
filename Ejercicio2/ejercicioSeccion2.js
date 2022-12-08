7b3171eb5251be09c9d4a32f0d76de5543f86c1f
David Soler <chantagosoler@gmail.com>
2022-12-08 14:15:25 -0500

    Ejercicio seccion 2

---
+++ Ejercicio2/ejercicioSeccion2.js
@@ -0,0 +1,9 @@
+const myfavoritebook = {
+    titulo: "globotomia",
+    autor: "Aramis Latchinian",
+    fecha: new Date(2009, 8),
+    url: "https://www.goodreads.com/es/book/show/12692601-globotomia"
+}
+
+const mylist = ["David", 17, true, new Date(2005, 2, 22), myfavoritebook]
+console.log(mylist)
